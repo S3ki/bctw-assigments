04d61c7567e27a8fc69cead7b4171a341036f32f
S3ki <92526584+S3ki@users.noreply.github.com>
2023-03-19 21:32:05 +0200

    week 1 node

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-"use strict" 
-
+'use strict';
 const _ = require('lodash');
 
 const output = _.camelCase('Foo Bar kukuku')
 console.log(output);
-console.log()
+console.log("kukkuu");
+console.log("taas jotain uutta");
+console.log("kukkuu");
+
+// ctrl-c sammuttaa nodemonin
