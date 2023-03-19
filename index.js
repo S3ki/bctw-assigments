430980307c4f2b08548df88e101aa00be3d7da25
S3ki <92526584+S3ki@users.noreply.github.com>
2023-03-19 20:31:29 +0200

    Branch week1 created

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-"use strict"
+"use strict" 
+
+const _ = require('lodash');
+
+const output = _.camelCase('Foo Bar kukuku')
+console.log(output);
+console.log()
---
+++ readme.md
@@ -0,0 +1,2 @@
+# BCTW Assignments
+
