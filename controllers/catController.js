492c94bdd10dc4ca29149b984946fdad361cf21d
S3ki <92526584+S3ki@users.noreply.github.com>
2023-03-20 12:23:52 +0200

    Configured Cat controller, Model and route

--- controllers/catController.js
+++ controllers/catController.js
@@ -1,2 +1,26 @@
 'use strict';
+
+const catModel = require('../models/catModel');
+
+const cats = catModel.cats;
+
+const { Module } = require("module");
+
 // catController
+
+const getCatList = (req, res) => {
+    res.json(cats);
+};
+
+const getCat = (req, res) => {
+    // console.log(req.params);
+    const id = req.params.catId
+    // Todo: filter matching cat by id
+    // TODO: response 404 if id not found in array (res.status(404))
+    const cat = cats[1]
+    res.json(cat);
+  };
+
+const catController = {getCatList, getCat};
+
+module.exports =  catController;
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,2 +1,23 @@
 'use strict';
-// catRoute
+const express = require('express');
+const router = express.Router();
+ const catController = require('../controllers/catController');
+
+router.get('/', catController.getCatList);
+
+router.get('/:catId', catController.getCat);
+
+//   TODO: move functions below controller
+  router.post('', (req, res) => {
+    res.send('From this endpoint you can add cats.')
+  });
+  
+  router.put('', (req, res) => {
+    res.send('From this endpoint you can modify a cat.')
+  });
+  
+  router.delete('', (req, res) => {
+    res.send('From this endpoint you can delete a cat.')
+  });
+
+module.exports = router;
